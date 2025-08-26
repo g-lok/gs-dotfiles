a869b00936343566931bb4d0f02c8e4771fd49d8
Garrett Anderson <g@devnull.rip>
2025-08-26 14:54:25 -0700

    splitting off macos

--- install.d/10-yos-screenshots.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Ignore if we're not on OSX
-[[ "$OSTYPE" =~ darwin* ]] || exit 0
-
-sudo -u $dotfiles_usr defaults write com.apple.screencapture location $dotfiles_usr_home/Pictures/Screenshots
-killall SystemUIServer
