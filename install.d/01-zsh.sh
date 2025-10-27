81933659f03f75cabdf82ed946cc8e0325a17717
Garrett Anderson <g@devnull.rip>
2025-10-26 20:00:28 -0700

    Updated Brewfile packages

---
+++ install.d/01-zsh.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+ZSH_CUSTOM="$HOME"/.oh-my-zsh/custom
+
+### Install zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM"/plugins/zsh-autosuggestions
+git clone https://github.com/Aloxaf/fzf-tab "$ZSH_CUSTOM"/plugins/fzf-tab
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM"/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git "$ZSH_CUSTOM"/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git "$ZSH_CUSTOM"/plugins/zsh-autocomplete
+git clone https://github.com/Aloxaf/fzf-tab "$ZSH_CUSTOM"/plugins/fzf-tab
--- install.sh
+++ install.sh
@@ -4,11 +4,20 @@
 set -x
 
 ## Personal set-up scripts for Ubuntu Linux and OSX
-## Heavily inspired (*ahem* copied) by https://github.com/Juxtaposedwords/dotfiles
+## Heavily inspired (*ahem* copied) by Juxtaposed, Omakub, too many to remember
 
 ## https://unix.stackexchange.com/questions/190571/sudo-in-non-interactive-script
 ## for idea on how to run sudo in script
 
+ascii_art='  _______/\        ________          __    _____.__.__                 
+ /  _____)/ ______ \______ \   _____/  |__/ ____\__|  |   ____   ______
+/   \  ___ /  ___/  |    |  \ /  _ \   __\   __\|  |  | _/ __ \ /  ___/
+\    \_\  \\___ \   |    `   (  <_> )  |  |  |  |  |  |_\  ___/ \___ \ 
+ \______  /____  > /_______  /\____/|__|  |__|  |__|____/\___  >____  >
+        \/     \/          \/                                \/     \/ '
+
+echo -e "$ascii_art"
+echo -e "\nBegin installation (or abort with ctrl+c)..."
 
 ## Detect the user who launched the script and save relevant info.
 ## (this might not actually be necessary)
@@ -17,16 +26,20 @@ export dotfiles_usr_home=$(sudo -u $dotfiles_usr echo $HOME)
 export dotfiles_wd=$(sudo -u $dotfiles_usr pwd)
 
 ## Exit if the script was not launched by root or through sudo
-if [ "$EUID" -ne 0 ]
-then
-    echo "The script needs to run as root" && exit 1
+if [ "$EUID" -ne 0 ]; then
+  echo "The script needs to run as root" && exit 1
 fi
 
-## Execute each dotfiles script
-for file in $dotfiles_wd/install.d/*.sh; do
-  chmod +x $file
-  bash $file
+## Make all scripts executable
+for file in "$dotfiles_wd"/install.d/*.sh; do
+  chmod +x "$file"
 done
+
+## Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+## Install gum
+brew install gum
 
 ## remove env variables
 unset dotfiles_usr
@@ -34,4 +47,3 @@ unset dotfiles_usr_home
 unset dotfiles_wd
 
 ## Set back to non-verbose output
-set +x
