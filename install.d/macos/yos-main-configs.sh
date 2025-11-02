dbe81a51b73561033f021ec4abbe018816098ed1
Garrett Anderson <g@devnull.rip>
2025-11-02 12:04:16 -0800

    trying to get SUDO_ASKPASS to work

--- install.d/macos/yos-main-configs.sh
+++ install.d/macos/yos-main-configs.sh
@@ -20,109 +20,109 @@ USER_ID=$(id -u "$CURRENT_USER")
 ############
 
 # Show battery percentage
-sudo -u "$CURRENT_USER" defaults -currentHost write com.apple.controlcenter BatteryShowPercentage -bool true
+sudo -A -u "$CURRENT_USER" defaults -currentHost write com.apple.controlcenter BatteryShowPercentage -bool true
 
 # Show date and seconds
-sudo -u "$CURRENT_USER" defaults write com.apple.menuextra.clock IsAnalog -int 0
-sudo -u "$CURRENT_USER" defaults write com.apple.menuextra.clock Show24Hour -int 1
-sudo -u "$CURRENT_USER" defaults write com.apple.menuextra.clock Date -int 0
-sudo -u "$CURRENT_USER" defaults write com.apple.menuextra.clock ShowDayOfWeek -int 1
-sudo -u "$CURRENT_USER" defaults write com.apple.menuextra.clock ShowSeconds -int 1
+sudo -A -u "$CURRENT_USER" defaults write com.apple.menuextra.clock IsAnalog -int 0
+sudo -A -u "$CURRENT_USER" defaults write com.apple.menuextra.clock Show24Hour -int 1
+sudo -A -u "$CURRENT_USER" defaults write com.apple.menuextra.clock Date -int 0
+sudo -A -u "$CURRENT_USER" defaults write com.apple.menuextra.clock ShowDayOfWeek -int 1
+sudo -A -u "$CURRENT_USER" defaults write com.apple.menuextra.clock ShowSeconds -int 1
 
 # Show bluetooth icon
-sudo -u "$CURRENT_USER" defaults -currentHost write com.apple.controlcenter Bluetooth -int 18
+sudo -A -u "$CURRENT_USER" defaults -currentHost write com.apple.controlcenter Bluetooth -int 18
 
 # Show volume icon
-sudo -u "$CURRENT_USER" defaults -currentHost write com.apple.controlcenter Sound -int 18
+sudo -A -u "$CURRENT_USER" defaults -currentHost write com.apple.controlcenter Sound -int 18
 
 # Disable Siri
-sudo -u "$CURRENT_USER" defaults write com.apple.Siri StatusMenuVisible -bool false
-sudo -u "$CURRENT_USER" defaults write com.apple.assistant.support "Assistant Enabled" -bool false
+sudo -A -u "$CURRENT_USER" defaults write com.apple.Siri StatusMenuVisible -bool false
+sudo -A -u "$CURRENT_USER" defaults write com.apple.assistant.support "Assistant Enabled" -bool false
 
 # Add VPN icon
-# sudo -u "$CURRENT_USER" open "/System/Library/CoreServices/Menu Extras/vpn.menu"
+# sudo -A -u "$CURRENT_USER" open "/System/Library/CoreServices/Menu Extras/vpn.menu"
 
 # Enable the input menu in the menu bar
-# sudo -u "$CURRENT_USER" defaults write com.apple.TextInputMenu visible -bool true
+# sudo -A -u "$CURRENT_USER" defaults write com.apple.TextInputMenu visible -bool true
 
 # Add an additional input source to the list of input sources. Ex: French - Numerical. Visible only after a reboot.
-# sudo -u "$CURRENT_USER" defaults write com.apple.HIToolbox AppleEnabledInputSources -array-add '<dict><key>InputSourceKind</key><string>Keyboard Layout</string><key>KeyboardLayout ID</key><integer>1111</integer><key>KeyboardLayout Name</key><string>French - numerical</string></dict>'
+# sudo -A -u "$CURRENT_USER" defaults write com.apple.HIToolbox AppleEnabledInputSources -array-add '<dict><key>InputSourceKind</key><string>Keyboard Layout</string><key>KeyboardLayout ID</key><integer>1111</integer><key>KeyboardLayout Name</key><string>French - numerical</string></dict>'
 
 ##########
 # FINDER #
 ##########
 
 # Show all filename extensions
-sudo -u "$CURRENT_USER" defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+sudo -A -u "$CURRENT_USER" defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
 # Show path bar
-sudo -u "$CURRENT_USER" defaults write com.apple.finder ShowPathbar -bool true
+sudo -A -u "$CURRENT_USER" defaults write com.apple.finder ShowPathbar -bool true
 
 # Show status bar
-sudo -u "$CURRENT_USER" defaults write com.apple.finder ShowStatusBar -bool true
+sudo -A -u "$CURRENT_USER" defaults write com.apple.finder ShowStatusBar -bool true
 
 # New Finder windows points to home
-sudo -u "$CURRENT_USER" defaults write com.apple.finder NewWindowTarget -string "PfHm"
+sudo -A -u "$CURRENT_USER" defaults write com.apple.finder NewWindowTarget -string "PfHm"
 
 # Disable the warning when changing a file extension
-sudo -u "$CURRENT_USER" defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+sudo -A -u "$CURRENT_USER" defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
 # Always show scrollbars. Possible values: `WhenScrolling`, `Automatic` and `Always`
-sudo -u "$CURRENT_USER" defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
+sudo -A -u "$CURRENT_USER" defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
 
 # Expand print panel by default
-sudo -u "$CURRENT_USER" defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-sudo -u "$CURRENT_USER" defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+sudo -A -u "$CURRENT_USER" defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+sudo -A -u "$CURRENT_USER" defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 
 # Avoid creating .DS_Store files on network or USB volumes
-sudo -u "$CURRENT_USER" defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-sudo -u "$CURRENT_USER" defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+sudo -A -u "$CURRENT_USER" defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+sudo -A -u "$CURRENT_USER" defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
 # Disable recent apps in Dock
-launchctl asuser "$USER_ID" sudo -u "$CURRENT_USER" defaults write com.apple.dock show-recents -int 0
+launchctl asuser "$USER_ID" sudo -A -u "$CURRENT_USER" defaults write com.apple.dock show-recents -int 0
 
 # Set custom wallpaper · Picture by https://basicappleguy.com
 # curl -o "/Users/Shared/wallpaper.png" "https://i.imgur.com/uonTUrp.png"
-# sudo -u "$CURRENT_USER" osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/Shared/wallpaper.png"'
+# sudo -A -u "$CURRENT_USER" osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/Shared/wallpaper.png"'
 
 # Display all files and folders sizes
-# sudo -u "$CURRENT_USER" /usr/libexec/PlistBuddy -c "Set :'FK_StandardViewSettings':'ExtendedListViewSettingsV2':'calculateAllSizes' 'true'" /Users/$CURRENT_USER/Library/Preferences/com.apple.finder.plist
-# sudo -u "$CURRENT_USER" /usr/libexec/PlistBuddy -c "Set :'FK_StandardViewSettings':'ListViewSettings':'calculateAllSizes' 'true'" /Users/$CURRENT_USER/Library/Preferences/com.apple.finder.plist
-# sudo -u "$CURRENT_USER" /usr/libexec/PlistBuddy -c "Set :'StandardViewSettings':'ExtendedListViewSettingsV2':'calculateAllSizes' 'true'" /Users/$CURRENT_USER/Library/Preferences/com.apple.finder.plist
-# sudo -u "$CURRENT_USER" /usr/libexec/PlistBuddy -c "Set :'StandardViewSettings':'ListViewSettings':'calculateAllSizes' 'true'" /Users/$CURRENT_USER/Library/Preferences/com.apple.finder.plist
+# sudo -A -u "$CURRENT_USER" /usr/libexec/PlistBuddy -c "Set :'FK_StandardViewSettings':'ExtendedListViewSettingsV2':'calculateAllSizes' 'true'" /Users/$CURRENT_USER/Library/Preferences/com.apple.finder.plist
+# sudo -A -u "$CURRENT_USER" /usr/libexec/PlistBuddy -c "Set :'FK_StandardViewSettings':'ListViewSettings':'calculateAllSizes' 'true'" /Users/$CURRENT_USER/Library/Preferences/com.apple.finder.plist
+# sudo -A -u "$CURRENT_USER" /usr/libexec/PlistBuddy -c "Set :'StandardViewSettings':'ExtendedListViewSettingsV2':'calculateAllSizes' 'true'" /Users/$CURRENT_USER/Library/Preferences/com.apple.finder.plist
+# sudo -A -u "$CURRENT_USER" /usr/libexec/PlistBuddy -c "Set :'StandardViewSettings':'ListViewSettings':'calculateAllSizes' 'true'" /Users/$CURRENT_USER/Library/Preferences/com.apple.finder.plist
 
 # Use current directory as default search scope in Finder
-sudo -u "$CURRENT_USER" defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+sudo -A -u "$CURRENT_USER" defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
 # Show hard drives and network drives on the Desktop
-# sudo -u "$CURRENT_USER" defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
-# sudo -u "$CURRENT_USER" defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+# sudo -A -u "$CURRENT_USER" defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+# sudo -A -u "$CURRENT_USER" defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
 
 ###################
 # SYSTEM SETTINGS #
 ###################
 
 # Beep feedback when changing volume
-# sudo -u "$CURRENT_USER" defaults write NSGlobalDomain com.apple.sound.beep.feedback -integer 1
+# sudo -A -u "$CURRENT_USER" defaults write NSGlobalDomain com.apple.sound.beep.feedback -integer 1
 
 # Beep feedback when plugging in the power adapter
-# sudo -u "$CURRENT_USER" defaults write com.apple.PowerChime ChimeOnAllHardware -bool true
+# sudo -A -u "$CURRENT_USER" defaults write com.apple.PowerChime ChimeOnAllHardware -bool true
 
 # Prevent Time Machine from prompting to use new hard drives as backup volume
-sudo -u "$CURRENT_USER" defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+sudo -A -u "$CURRENT_USER" defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
 # Expand save panel by default
-sudo -u "$CURRENT_USER" defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+sudo -A -u "$CURRENT_USER" defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 
 # Disable Force Click / Force Touch / Haptic feedback
-sudo -u "$CURRENT_USER" defaults write com.apple.AppleMultitouchTrackpad ForceSuppressed -int 1
+sudo -A -u "$CURRENT_USER" defaults write com.apple.AppleMultitouchTrackpad ForceSuppressed -int 1
 
 # Enable two buttons / secondary / right click on Magic Mouses
-sudo -u "$CURRENT_USER" defaults write com.apple.AppleMultitouchMouse MouseButtonMode -string "TwoButton"
-sudo -u "$CURRENT_USER" defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode -string "TwoButton"
+sudo -A -u "$CURRENT_USER" defaults write com.apple.AppleMultitouchMouse MouseButtonMode -string "TwoButton"
+sudo -A -u "$CURRENT_USER" defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode -string "TwoButton"
 
 # Prevent Photos.app from opening when a device is plugged
-sudo -u "$CURRENT_USER" defaults write com.apple.ImageCapture disableHotPlug -bool yes
+sudo -A -u "$CURRENT_USER" defaults write com.apple.ImageCapture disableHotPlug -bool yes
 
 # Turn off Bluetooth if it's a laptop
 # LAPTOPMODEL=$(/usr/sbin/system_profiler SPHardwareDataType | /usr/bin/grep "Model Name" | /usr/bin/grep "Book" | /usr/bin/awk -F ": " '{print $2}')
@@ -134,7 +134,7 @@ sudo -u "$CURRENT_USER" defaults write com.apple.ImageCapture disableHotPlug -bo
 # fi
 
 # Disable shadows on screenshots
-sudo -u "$CURRENT_USER" defaults write com.apple.screencapture disable-shadow -bool true
+sudo -A -u "$CURRENT_USER" defaults write com.apple.screencapture disable-shadow -bool true
 
 # Enable automatic updates
 defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist AutomaticallyInstallMacOSUpdates -bool true
@@ -145,10 +145,10 @@ defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist ConfigDataIns
 defaults write /Library/Preferences/com.apple.commerce.plist AutoUpdate -bool true
 
 # Change Terminal settings
-sudo -u "$CURRENT_USER" defaults write com.apple.Terminal "Default Window Settings" -string "Pro"
-sudo -u "$CURRENT_USER" defaults write com.apple.Terminal "Startup Window Settings" -string "Pro"
-sudo -u "$CURRENT_USER" /usr/libexec/PlistBuddy -c "Add :'Window Settings':'Pro':'CursorBlink' bool true" /Users/"$CURRENT_USER"/Library/Preferences/com.apple.Terminal.plist
-sudo -u "$CURRENT_USER" /usr/libexec/PlistBuddy -c "Set :'Window Settings':'Pro':'CursorBlink' 'true'" /Users/"$CURRENT_USER"/Library/Preferences/com.apple.Terminal.plist
+sudo -A -u "$CURRENT_USER" defaults write com.apple.Terminal "Default Window Settings" -string "Pro"
+sudo -A -u "$CURRENT_USER" defaults write com.apple.Terminal "Startup Window Settings" -string "Pro"
+sudo -A -u "$CURRENT_USER" /usr/libexec/PlistBuddy -c "Add :'Window Settings':'Pro':'CursorBlink' bool true" /Users/"$CURRENT_USER"/Library/Preferences/com.apple.Terminal.plist
+sudo -A -u "$CURRENT_USER" /usr/libexec/PlistBuddy -c "Set :'Window Settings':'Pro':'CursorBlink' 'true'" /Users/"$CURRENT_USER"/Library/Preferences/com.apple.Terminal.plist
 
 # Startup Chime / StartupMute=%01 to mute
 nvram StartupMute=%00
@@ -175,13 +175,13 @@ defaults write /private/var/db/timed/Library/Preferences/com.apple.timed.plist T
 # /usr/sbin/systemsetup -settimezone America/New_York
 
 # Safari : show bookmarks bar and status bar
-launchctl asuser "$USER_ID" sudo -u "$CURRENT_USER" defaults write com.apple.Safari ShowFavoritesBar-v2 -bool true
-launchctl asuser "$USER_ID" sudo -u "$CURRENT_USER" defaults write com.apple.Safari ShowOverlayStatusBar -bool true
+launchctl asuser "$USER_ID" sudo -A -u "$CURRENT_USER" defaults write com.apple.Safari ShowFavoritesBar-v2 -bool true
+launchctl asuser "$USER_ID" sudo -A -u "$CURRENT_USER" defaults write com.apple.Safari ShowOverlayStatusBar -bool true
 
 # Google Chrome : Force relaunch (in milliseconds) when there is a pending update + Home button
-launchctl asuser "$USER_ID" sudo -u "$CURRENT_USER" defaults write com.google.Chrome RelaunchNotificationPeriod -int 21600000
-launchctl asuser "$USER_ID" sudo -u "$CURRENT_USER" defaults write com.google.Chrome RelaunchNotification -int 2
-launchctl asuser "$USER_ID" sudo -u "$CURRENT_USER" defaults write com.google.Chrome ShowHomeButton -bool true
+launchctl asuser "$USER_ID" sudo -A -u "$CURRENT_USER" defaults write com.google.Chrome RelaunchNotificationPeriod -int 21600000
+launchctl asuser "$USER_ID" sudo -A -u "$CURRENT_USER" defaults write com.google.Chrome RelaunchNotification -int 2
+launchctl asuser "$USER_ID" sudo -A -u "$CURRENT_USER" defaults write com.google.Chrome ShowHomeButton -bool true
 
 # Remove Wi-Fi SSID from known networks. Ex: forget the Wi-Fi dedicated to Mac enrollments
 for interface in $(networksetup -listnetworkserviceorder | grep Hardware | awk '/Wi-Fi/ { print $NF }' | awk -F ")" '{ print $1 }'); do
@@ -190,11 +190,11 @@ for interface in $(networksetup -listnetworkserviceorder | grep Hardware | awk '
 done
 
 # Activity Monitor : Show Data in graph instead of IO and packets
-sudo -u "$CURRENT_USER" defaults write com.apple.ActivityMonitor DiskGraphType -int 1
-sudo -u "$CURRENT_USER" defaults write com.apple.ActivityMonitor NetworkGraphType -int 1
+sudo -A -u "$CURRENT_USER" defaults write com.apple.ActivityMonitor DiskGraphType -int 1
+sudo -A -u "$CURRENT_USER" defaults write com.apple.ActivityMonitor NetworkGraphType -int 1
 
 # Uncheck 'Reopen Windows When Logging Back'
-sudo -u "$CURRENT_USER" defaults write com.apple.loginwindow TALLogoutSavesState -bool false
+sudo -A -u "$CURRENT_USER" defaults write com.apple.loginwindow TALLogoutSavesState -bool false
 
 # Enable Firewall
 # /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
@@ -208,29 +208,29 @@ pmset -a powernap 0
 pmset -a womp 0
 
 # Touch Bar shows Expanded Control Strip
-sudo -u "$CURRENT_USER" defaults write com.apple.touchbar.agent PresentationModeGlobal -string "fullControlStrip"
+sudo -A -u "$CURRENT_USER" defaults write com.apple.touchbar.agent PresentationModeGlobal -string "fullControlStrip"
 
-# Touch ID for sudo commands in Terminal · Ventura + Monterey
-# sed -i '.bak' '2s/^/auth       sufficient     pam_tid.so\'$'\n/g' /private/etc/pam.d/sudo
+# Touch ID for sudo -A commands in Terminal · Ventura + Monterey
+# sed -i '.bak' '2s/^/auth       sufficient     pam_tid.so\'$'\n/g' /private/etc/pam.d/sudo -A
 
-# Touch ID for sudo commands in Terminal · Sonoma + Sequoia
-CHECK_3RD_ROW=$(cat /private/etc/pam.d/sudo_local | sed -n 3p)
+# Touch ID for sudo -A commands in Terminal · Sonoma + Sequoia
+CHECK_3RD_ROW=$(cat /private/etc/pam.d/sudo -A_local | sed -n 3p)
 if [ "$CHECK_3RD_ROW" = "auth       sufficient     pam_tid.so" ]; then
-  echo "Sudo Touch ID is already configured."
+  echo "sudo -A Touch ID is already configured."
 else
-  cp /private/etc/pam.d/sudo_local.template /private/etc/pam.d/sudo_local
-  sed -i '.bak' '3s/#//' "/private/etc/pam.d/sudo_local"
-  echo "Sudo Touch ID is now configured."
+  cp /private/etc/pam.d/sudo -A_local.template /private/etc/pam.d/sudo -A_local
+  sed -i '.bak' '3s/#//' "/private/etc/pam.d/sudo -A_local"
+  echo "sudo -A Touch ID is now configured."
 fi
 
 # Remove login items
-# sudo -u "$CURRENT_USER" /usr/bin/osascript -e 'tell application "System Events" to delete login item "Skype for Business"'
+# sudo -A -u "$CURRENT_USER" /usr/bin/osascript -e 'tell application "System Events" to delete login item "Skype for Business"'
 
 # Set Slack workspace ID : https://slack.com/help/articles/221769328-Locate-your-Slack-URL-or-ID#find-your-workspace-or-org-id
-# sudo -u "$CURRENT_USER" defaults write com.tinyspeck.slackmacgap SlackDefaultSignInWorkspace -string 'TXXXXXX'
+# sudo -A -u "$CURRENT_USER" defaults write com.tinyspeck.slackmacgap SlackDefaultSignInWorkspace -string 'TXXXXXX'
 
 # Disable tiled windows margins
-sudo -u "$CURRENT_USER" defaults write com.apple.WindowManager EnableTiledWindowMargins -bool false
+sudo -A -u "$CURRENT_USER" defaults write com.apple.WindowManager EnableTiledWindowMargins -bool false
 
 # Stop macOS 15 Sequoia monthly screen recording prompts for Chrome, Slack, Zoom, Teams and TeamViewer
 defaults write "/Users/$CURRENT_USER/Library/Group Containers/group.com.apple.replayd/ScreenCaptureApprovals.plist" "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" -date "3024-09-22 12:12:12 +0000"
