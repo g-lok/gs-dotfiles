3dd236f35b0abccf4b69f296a68d5ec356b23191
Garrett Anderson <g@devnull.rip>
2025-11-10 13:12:38 -0800

    debug line by line

--- install.d/macos/yos-packages.sh
+++ install.d/macos/yos-packages.sh
@@ -61,6 +61,7 @@ install_alacritty() {
   alarcirtty_url="https://github.com/alacritty/alacritty/releases/download/v0.16.1/Alacritty-v0.16.1.dmg"
   curl -o alaritty.dmg "$alarcirtty_url"
   printf "$HOMEBREW_PASSWORD" | hdiutil attach -stdinpass "alacritty.dmg"
+  printf "$HOMEBREW_PASSWORD" | sudo -S cp -R "/Volumes/Alacritty/Alacritty.app" "/Applications/"
   rm "alacritty.dmg"
 
   o
