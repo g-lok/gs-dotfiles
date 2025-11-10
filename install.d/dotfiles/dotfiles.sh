97e5afbac40a4c67502b4d8d995b773713279c66
Garrett Anderson <g@devnull.rip>
2025-11-10 12:21:11 -0800

    debug line by line

--- install.d/dotfiles/dotfiles.sh
+++ install.d/dotfiles/dotfiles.sh
@@ -8,7 +8,7 @@ cd "$(dirname "$0")" || exit 0
 export DOTFILES_SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
 
 ## GNU STow to set up configs
-export STOW_CONFIGS=dirs=(*/)
+export STOW_CONFIGS=(*/)
 echo $STOW_CONFIGS
 for config in "${STOW_CONFIGS[@]}"; do
   ## stow adopt it first before anything to keep the user's existing configs safe
