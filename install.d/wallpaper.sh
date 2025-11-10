9766ebf0c8e76c6902d79e97387e1675aa1fedd1
Garrett Anderson <g@devnull.rip>
2025-11-09 17:34:51 -0800

    try to fix furnace installation

--- install.d/wallpaper.sh
+++ install.d/wallpaper.sh
@@ -7,7 +7,7 @@ case "$SCRIPT_OS" in
   command='tell application "Finder" to set desktop picture to POSIX file '
   command_w_file="$command$HOME/Pictures/Wallpapers/victorian-wallpaper.jpg"
   echo "$command_w_file"
-  osascript -e $command_w_file
+  osascript -e "$command_w_file"
   ;;
 "linux_gui")
   gsettings set org.gnome.desktop.background picture-uri "file://$HOME/Pictures/wallpaper.jpg"
