5c0b57dcfa9a683e4322f8b254d838e6c52500eb
Garrett Anderson <g@devnull.rip>
2025-11-09 17:53:01 -0800

    fixed wallpaper script

--- install.d/wallpaper.sh
+++ install.d/wallpaper.sh
@@ -4,10 +4,7 @@ cp "$GS_DOTFILES_PATH/themes/victorian-wallpaper.jpg" ~/Pictures/Wallpapers/
 
 case "$SCRIPT_OS" in
 "MacOS")
-  command='tell application "Finder" to set desktop picture to POSIX file '
-  command_w_file="$command$HOME/Pictures/Wallpapers/victorian-wallpaper.jpg"
   filepath="$HOME/Pictures/Wallpapers/victorian-wallpaper.jpg"
-  echo "$command_w_file"
   echo "$filepath"
   # osascript -e "$command_w_file"
   osascript -e 'tell application "Finder" to set desktop picture to POSIX file "'"$filepath"'"'
