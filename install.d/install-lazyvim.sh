a42b05bc9c69233f803734beefef19c14879b5d4
Garrett Anderson <g@devnull.rip>
2025-11-11 14:33:59 -0800

    debug line by line

--- install.d/install-lazyvim.sh
+++ install.d/install-lazyvim.sh
@@ -3,12 +3,22 @@
 
 ## Backup current neovim configs
 # required
-mv ~/.config/nvim{,.bak}
+if [ -d ~/.config/nvim ]; then
+  mv ~/.config/nvim{,.bak}
+fi
 
 # optional but recommended
-mv ~/.local/share/nvim{,.bak}
-mv ~/.local/state/nvim{,.bak}
-mv ~/.cache/nvim{,.bak}
+if [ -d ~/.local/share/nvim ]; then
+  mv ~/.local/share/nvim{,.bak}
+fi
+
+if [ -d ~/local/state/nvim ]; then
+  mv ~/.local/state/nvim{,.bak}
+fi
+
+if [ -d ~/.cache/nvim ]; then
+  mv ~/.cache/nvim{,.bak}
+fi
 
 ## Install and remove .git folder
 git clone https://github.com/LazyVim/starter ~/.config/nvim
