a7197c4386c2b5318ccd33282c11d8572ad800e0
Garrett Anderson <g@devnull.rip>
2025-11-11 15:15:42 -0800

    debug line by line

--- install.d/macos/yos-peripherals.sh
+++ install.d/macos/yos-peripherals.sh
@@ -3,11 +3,13 @@
 ## Ignore if we're not on OSX
 [[ "$OSTYPE" =~ darwin* ]] || exit 0
 
+export CURRENT_USER=$(stat -f %Su /dev/console)
+export USER_ID=$(id -u "$CURRENT_USER")
 ###############################################################################
 # Mice, keyboard, etc                                                         #
 ###############################################################################
 ## Set a blazingly fast keyboard repeat rate
-sudo -u "$CURRENT_USER" defaults write NSGlobalDomain KeyRepeat -int 0
+sudo -A -u "$CURRENT_USER" defaults write NSGlobalDomain KeyRepeat -int 0
 
 # disable Natural scrolling
 # sudo -u "$CURRENT_USER" defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
