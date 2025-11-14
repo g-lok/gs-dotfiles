ab11f6ba318cb36d902ca88b59ccd664fd683339
Garrett Anderson <g@devnull.rip>
2025-11-14 07:07:20 -0800

    comment out set e

--- install.sh
+++ install.sh
@@ -1,42 +1,39 @@
 #!/usr/bin/env bash
 
-set -e
+# set -e
 
 ## Personal set-up scripts for Ubuntu Linux and OSX
 ## Heavily inspired (*ahem* copied) by Juxtaposed, Omakub, too many to remember
 
 ## Print Ascii logo with color.
 ascii_art='
-  _______/\        ________          __    _____.__.__                 
+  _______/\        ________          __    _____.__.__
  /  _____)/ ______ \______ \   _____/  |__/ ____\__|  |   ____   ______
 /   \  ___ /  ___/  |    |  \ /  _ \   __\   __\|  |  | _/ __ \ /  ___/
-\    \_\  \\___ \   |    `   (  <_> )  |  |  |  |  |  |_\  ___/ \___ \ 
+\    \_\  \\___ \   |    `   (  <_> )  |  |  |  |  |  |_\  ___/ \___ \
  \______  /____  > /_______  /\____/|__|  |__|  |__|____/\___  >____  >
         \/     \/          \/                                \/     \/
-
-
 '
 
 ## Define the color gradient (shades of cyan and blue)
 colors=(
-	'\033[38;5;81m' # Cyan
-	'\033[38;5;75m' # Light Blue
-	'\033[38;5;69m' # Sky Blue
-	'\033[38;5;63m' # Dodger Blue
-	'\033[38;5;57m' # Deep Sky Blue
-	'\033[38;5;51m' # Cornflower Blue
-	'\033[38;5;45m' # Royal Blue
+  '\033[38;5;81m' # Cyan
+  '\033[38;5;75m' # Light Blue
+  '\033[38;5;69m' # Sky Blue
+  '\033[38;5;63m' # Dodger Blue
+  '\033[38;5;57m' # Deep Sky Blue
+  '\033[38;5;51m' # Cornflower Blue
+  '\033[38;5;45m' # Royal Blue
 )
-
-## Split the ASCII art into lines
+#
+# ## Split the ASCII art into lines
 IFS=$'\n' read -rd '' -a lines <<<"$ascii_art"
-
-## Print each line with the corresponding color
+#
+# ## Print each line with the corresponding color
 for i in "${!lines[@]}"; do
-	color_index=$((i % ${#colors[@]}))
-	echo -e "${colors[color_index]}${lines[i]}"
+  color_index=$((i % ${#colors[@]}))
+  echo -e "${colors[color_index]}${lines[i]}"
 done
-
 
 echo -e "\nBegin installation (or abort with ctrl+c)..."
 
