9e30ade1d0e0534c688a2a543dc0f2e8cdc1f156
Garrett Anderson <g@devnull.rip>
2025-11-15 16:14:49 -0800

    break out alacritty dotfiles

--- install.d/dotfiles/dotfiles.sh
+++ install.d/dotfiles/dotfiles.sh
@@ -7,14 +7,46 @@ cd "$(dirname "$0")" || exit 0
 ## Get the script path while we're at it
 export DOTFILES_SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
 
-## GNU STow to set up configs
-export STOW_CONFIGS=(*/)
-echo $STOW_CONFIGS
-for config in "${STOW_CONFIGS[@]}"; do
+## We doing this precise and intentional baby
+UNIVERSAL_PACKAGES=(
+  "bash"
+  "btop"
+  "shellrc"
+  "VSCode"
+  "zellij"
+  "zsh"
+)
+
+## GNU Stow to set up non-OS specific configs
+for config in "${UNIVERSAL_PACKAGES[@]}"; do
   ## stow adopt it first before anything to keep the user's existing configs safe
   stow --target="$HOME" --adopt "$config"
   ## stow restow to make it clean
   stow --target="$HOME" --restow "$config"
 done
 
-# stow --target="$HOME" --restow $DOTFILES_SCRIPT_PATH/dotfiles/*
+OS=$(uname -s)
+case "$OS" in
+"Linux")
+  stow --target="$HOME" --adopt "alacritty-linux"
+  stow --target="$HOME" --restow "alacritty-linux"
+  ;;
+"Darwin")
+  if [[ "$(uname -m)" == "arm64" ]]; then
+    CHIPSET="ARM64"
+  else
+    CHIPSET="INTEL"
+  fi
+
+  if [[ $CHIPSET == "ARM64" ]]; then
+    stow --target="$HOME" --adopt "alacritty-macos-arm"
+    stow --target="$HOME" --restow "alacritty-macos-arm"
+  elif [[ $CHIPSET == "INTEL" ]]; then
+    stow --target="$HOME" --adopt "alacritty-macos-intel"
+    stow --target="$HOME" --restow "alacritty-macos-intel"
+  fi
+  ;;
+*)
+  return
+  ;;
+esac
