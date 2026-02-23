0ad2af99ebec63fa74cd5140298969d4a1869e93
Garrett Anderson <g@devnull.rip>
2026-02-23 04:57:33 -0800

    Get rid of extra banner

--- install.sh
+++ install.sh
@@ -9,23 +9,6 @@ export GSDOT_PATH="$HOME/.local/share/gsdotfiles"
 export GSDOT_SCRIPTS="$GSDOT_PATH/install.d"
 export GSDOT_DOTFILES="$GSDOT_PATH/install.d/dotfiles"
 
-## Print logo
-case $BASH_VERSION in
-4.* | 5.* | 6.*)
-  source "$GSDOT_SCRIPTS/ascii.sh"
-  ;;
-*)
-  cat <<'EOF'
-  ________                 ________          __    _____.__.__                 
- /  _____/  ______         \______ \   _____/  |__/ ____\__|  |   ____   ______
-/   \  ___ /  ___/  ______  |    |  \ /  _ \   __\   __\|  |  | _/ __ \ /  ___/
-\    \_\  \\___ \  /_____/  |    `   (  <_> )  |  |  |  |  |  |_\  ___/ \___ \ 
- \______  /____  >         /_______  /\____/|__|  |__|  |__|____/\___  >____  >
-        \/     \/                  \/                                \/     \/
-EOF
-  ;;
-esac
-
 ## Begin
 echo -e "\nBegin installation (or abort with ctrl+c)..."
 
