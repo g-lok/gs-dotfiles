d16ee8ccb1d0c95e7932739edd2ac0c87e82ac6d
Garrett Anderson <g@devnull.rip>
2026-02-23 06:14:33 -0800

    I hate gum and bash so much i hate them

--- install.d/macos/launch.sh
+++ install.d/macos/launch.sh
@@ -11,19 +11,20 @@ source "$GSDOT_SCRIPTS/install-lazyvim.sh" >/dev/null 2>&1
 # gum spin --spinner moon --title "Configuring System Settings..." -- "$GSDOT_SCRIPTS/macos/yos-main-configs.sh"
 # gum spin --spinner moon --title "Configuring main MacOS settings..." -- sleep 2
 source "$GSDOT_SCRIPTS/macos/yos-main-configs.sh" >/dev/null 2>&1
-gum spin --spinner moon --title "Configure Dock Settings" -- "$GSDOT_SCRIPTS/macos/yos-dock.sh"
+# gum spin --spinner moon --title "Configure Dock Settings" -- "$GSDOT_SCRIPTS/macos/yos-dock.sh"
 # gum spin --spinner moon --title "Configuring Dock settings..." -- sleep 2
-# source "$GSDOT_SCRIPTS/macos/yos-dock.sh"
-gum spin --spinner moon --title "Configure Peripheral Settings..." -- "$GSDOT_SCRIPTS/macos/yos-peripherals.sh"
+source "$GSDOT_SCRIPTS/macos/yos-dock.sh"
+# gum spin --spinner moon --title "Configure Peripheral Settings..." -- "$GSDOT_SCRIPTS/macos/yos-peripherals.sh"
 # gum spin --spinner moon --title "Configuring peripherals..." -- sleep 2
-# source "$SCRIPTS_DIR/macos/yos-peripherals.sh" >/dev/null 2>&1
-gum spin --spinner moon --title "Configuring screenshots..." -- "$GSDOT_SCRIPTS/macos/yos-screenshots.sh"
+source "$GSDOT_SCRIPTS/macos/yos-peripherals.sh" >/dev/null 2>&1
+# gum spin --spinner moon --title "Configuring screenshots..." -- "$GSDOT_SCRIPTS/macos/yos-screenshots.sh"
 # gum spin --spinner moon --title "Configuring screenshots..." -- sleep 2
-# source "$SCRIPTS_DIR/macos/yos-screenshots.sh" >/dev/null 2>&1
-gum spin --spinner moon --title "Installing theme..." -- "$GSDOT_SCRIPTS/themes.sh"
+source "$GSDOT_SCRIPTS/macos/yos-screenshots.sh" >/dev/null 2>&1
+source "$GSDOT_SCRIPTS/themes.sh" >/dev/null 2>&1
+# gum spin --spinner moon --title "Installing theme..." -- "$GSDOT_SCRIPTS/themes.sh"
 # gum spin --spinner moon --title "Configuring Dotfiles..." -- "$GSDOT_SCRIPTS/dotfiles/dotfiles.sh"
 # gum spin --spinner moon --title "Configuring dotfiles..." -- sleep 2
 "$GSDOT_SCRIPTS/dotfiles/dotfiles.sh" >/dev/null 2>&1
-gum spin --spinner moon --title "Configuring Git..." -- "$GSDOT_SCRIPTS/git.sh"
+# gum spin --spinner moon --title "Configuring Git..." -- "$GSDOT_SCRIPTS/git.sh"
 # gum spin --spinner moon --title "Configuring git..." -- sleep 2
-# source "$SCRIPTS_DIR/git.sh" >/dev/null 2>&1
+source "$GSDOT_SCRIPTS/git.sh" >/dev/null 2>&1
