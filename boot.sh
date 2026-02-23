b65a638762640d2dacf8d2c1e557ce59c66fdafb
Garrett Anderson <g@devnull.rip>
2026-02-23 01:14:34 -0800

    Working toward db/docker functionality

--- boot.sh
+++ boot.sh
@@ -1,1 +1,32 @@
 #!/usr/bin/env bash
+
+## Print logo
+case $BASH_VERSION in
+4.* | 5.* | 6.*)
+  source "$GSDOT_SCRIPTS/ascii.sh"
+  ;;
+*)
+  cat <<'EOF'
+  ________                 ________          __    _____.__.__                 
+ /  _____/  ______         \______ \   _____/  |__/ ____\__|  |   ____   ______
+/   \  ___ /  ___/  ______  |    |  \ /  _ \   __\   __\|  |  | _/ __ \ /  ___/
+\    \_\  \\___ \  /_____/  |    `   (  <_> )  |  |  |  |  |  |_\  ___/ \___ \ 
+ \______  /____  >         /_______  /\____/|__|  |__|  |__|____/\___  >____  >
+        \/     \/                  \/                                \/     \/
+EOF
+  ;;
+esac
+
+## Begin
+echo -e "\nBegin installation (or abort with ctrl+c)..."
+echo "Cloning GSDotfiles..."
+rm -rf ~/.local/share/gsdotfiles
+git clone https://github.com/g-lok/gs-dotfiles.git ~/.local/share/gsdotfiles >/dev/null
+# if [[ $OMAKUB_REF != "master" ]]; then
+# 	cd ~/.local/share/gsdotfiles || exit 0
+# 	git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
+# 	cd -
+# fi
+
+echo "Installation starting..."
+source ~/.local/share/gsdotfiles/install.sh
