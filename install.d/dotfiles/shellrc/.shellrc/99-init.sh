1377d3ddb5b983aa64c7698009cfcd04d5c0ecb3
Garrett Anderson <g@devnull.rip>
2026-02-26 15:50:15 -0800

    Get rid of carapace, it conflicts with fzf-tab

--- install.d/dotfiles/shellrc/.shellrc/99-init.sh
+++ install.d/dotfiles/shellrc/.shellrc/99-init.sh
@@ -11,21 +11,6 @@ if command -v starship &>/dev/null; then
   eval "$(starship init "$myshell")"
 fi
 
-if command -v carapace &>/dev/null; then
-  export UserConfigDir="$HOME/.config"
-  case $myshell in
-  bash)
-    export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense' # optional
-    source <(carapace _carapace)
-    ;;
-  zsh)
-    export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense' # optional
-    zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
-    source <(carapace _carapace)
-    ;;
-  esac
-fi
-
 if command -v mise &>/dev/null; then
   eval "$(mise activate "$myshell")"
 fi
