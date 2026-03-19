474938bad95c9b06a362a44f6428d363db6ce01f
Garrett Anderson <g@devnull.rip>
2026-03-18 20:51:13 -0700

    disable snacks explorer, add text-case, and others

---
+++ install.d/dotfiles/neovim/.config/nvim/lua/plugins/disable.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		{
+			"snacks.nvim",
+			opts = {
+				scroll = { enabled = false },
+				explorer = { enabled = false },
+			},
+			keys = {
+				{ "e", false },
+				{ "E", false },
+				{ "fe", false },
+				{ "fE", false },
+			},
+		},
+	},
+}
